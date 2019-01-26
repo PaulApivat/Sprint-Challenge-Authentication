8bb30aa4c8e6a860201bf40b49867723b90a827f
paulapivat <paul.apivat@gmail.com>
2019-01-27 02:04:12 +0700

    MVP complete, add authentication, used hardcoded secret key instead of jwtKey

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config(); // load .env variables
 
 // const { server } = require('./api/server.js');
-
+const axios = require('axios');
 
 //************** */
 const express = require('express');
@@ -9,6 +9,16 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const jwt = require('jsonwebtoken');
+// required jwtKey
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'add a .env file to root of project with the JWT_SECRET variable';
+
+// quickly see what this file exports
+module.exports = {
+  authenticate,
+};
+
 
 const knex = require('knex');
 const knexConfig = require('./knexfile.js');
@@ -81,6 +91,72 @@ server.post('/api/login', (req, res) => {
       })
 });
 
+// ADDING AUTHENTICATION
+// function protect(req, res, next){
+//   // ust jwt and read token string from Authorization header
+//   const token = req.headers.authorization;
+
+//   if(token){
+//     jwt.verify(token, secret, (err, decodedToken) => {
+//       if(err){
+//         //token is invalid 
+//         res.status(401).json({ message: "Invalid Token" });
+//       } else {
+//         //token is valid
+//         console.log(decodedToken)
+//         req.username = decodedToken.username;
+//         next();
+//       }
+//     });
+//   } else {
+//     res.status(401).json({message: 'no token provided '});
+//   }
+// }
+
+// GIVEN AUTHENTICATION
+function authenticate(req, res, next) {
+  const token = req.get('Authorization');
+
+  if (token) {
+    jwt.verify(token, secret, (err, decoded) => {     //jwtKey
+      if (err) return res.status(401).json(err);
+
+      req.decoded = decoded;
+
+      next();
+    });
+  } else {
+    return res.status(401).json({
+      error: 'No token provided, must be set on the Authorization Header',
+    });
+  }
+}
+
+server.get('/api/jokes', authenticate, (req, res) => {
+  const requestOptions = {
+    headers: { accept: 'application/json' },
+  };
+
+  axios
+    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .then(response => {
+      res.status(200).json(response.data.results);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+    });
+})
+
+
+// server.get('/api/jokes', protect, (req, res) => {
+//   db('users').select('id', 'username', 'password')
+//   .then(users => {
+//     res.json(users);
+//   })
+//   .catch(err => {
+//     res.status(500).send(err);
+//   })
+// })
 
 server.get('/', (req , res) => {
   res.send('Sprint Challenge Authentication...')
